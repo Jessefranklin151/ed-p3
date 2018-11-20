9ba00f8d4df27ea9ff9bd2f5c9709183636c6a5a
Jessé Franklin <jesseoito@gmail.com>
2018-11-20 17:03:32 -0300

    iniciando a fila encadeada

---
+++ src/br/com/facisa/p3/fila/FilaEncadeada.java
@@ -0,0 +1,29 @@
+package br.com.facisa.p3.fila;
+
+import br.com.facisa.p3.Item;
+
+public class FilaEncadeada {
+
+    private Item primeiroItem;
+    private int contador;
+
+    public void enqueue(Item item) {
+
+    }
+
+    public Item dequeue() {
+	return null;
+    }
+
+    public Item front() {
+	return null;
+    }
+
+    public int size() {
+	return contador;
+    }
+
+    public boolean isEmpty() {
+	return contador == 0;
+    }
+}

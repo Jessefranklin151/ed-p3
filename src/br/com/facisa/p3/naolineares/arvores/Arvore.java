d81cc33aa0fbc48ec4dd8fcb7dcba414865fc0dc
jose.sobrinho <jesseoito@gmail.com>
2018-11-21 12:16:42 -0300

    adicionar da arvore

--- src/br/com/facisa/p3/naolineares/arvores/Arvore.java
+++ src/br/com/facisa/p3/naolineares/arvores/Arvore.java
@@ -5,18 +5,17 @@ import br.com.facisa.p3.naolineares.Node;
 public class Arvore {
 
 	private Node raiz;
-	
+
 	public void adicionar(Integer valor) {
 		adicionar(this.raiz, valor);
 	}
-	
-	
+
 	public void adicionar(Node node, Integer valor) {
-		
+
 		if (raiz == null) {
 			this.raiz = new Node(valor);
 		} else {
-			
+
 			if (valor > node.getValor()) {
 				if (node.getDireita() == null) {
 					node.setDireita(new Node(valor));
@@ -31,6 +30,13 @@ public class Arvore {
 				}
 			}
 		}
-		
+
 	}
+
+	public void emOrdem() {
+		// vai pelo meio.
+	}
+
+	// TODO: imprimir emOrdem, preOrdem, posOrdem, exibirRaiz.
+	// TODO: removeMinimo, encontraMinimo, remover(Node, valor).
 }
